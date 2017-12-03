820d9723e5e7cd0672b5c01c3e3dc0734e3f25dd
Emmax77 <emmanuelguzman96@gmail.com>
2017-12-03 15:16:39 -0600

    Avances

--- ProyectoPatrono/ProyectoPatrono/Gestor.Designer.cs
+++ ProyectoPatrono/ProyectoPatrono/Gestor.Designer.cs
@@ -32,6 +32,8 @@
             this.lbDatosRecibidos = new System.Windows.Forms.Label();
             this.btnCargar = new System.Windows.Forms.Button();
             this.OFD = new System.Windows.Forms.OpenFileDialog();
+            this.btnXML = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDatosBanco)).BeginInit();
             this.SuspendLayout();
             // 
@@ -58,11 +60,11 @@
             // btnCargar
             // 
             this.btnCargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCargar.Location = new System.Drawing.Point(248, 376);
+            this.btnCargar.Location = new System.Drawing.Point(12, 380);
             this.btnCargar.Name = "btnCargar";
-            this.btnCargar.Size = new System.Drawing.Size(122, 28);
+            this.btnCargar.Size = new System.Drawing.Size(159, 28);
             this.btnCargar.TabIndex = 2;
-            this.btnCargar.Text = "Cargar Archivo";
+            this.btnCargar.Text = "Cargar Archivo Excel";
             this.btnCargar.UseVisualStyleBackColor = true;
             this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
             // 
@@ -71,11 +73,35 @@
             this.OFD.FileName = "OpenFileDialog1";
             this.OFD.FileOk += new System.ComponentModel.CancelEventHandler(this.OFD_FileOk_1);
             // 
+            // btnXML
+            // 
+            this.btnXML.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXML.Location = new System.Drawing.Point(237, 380);
+            this.btnXML.Name = "btnXML";
+            this.btnXML.Size = new System.Drawing.Size(159, 28);
+            this.btnXML.TabIndex = 3;
+            this.btnXML.Text = "Cargar Archivo XML";
+            this.btnXML.UseVisualStyleBackColor = true;
+            this.btnXML.Click += new System.EventHandler(this.btnXML_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(451, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Cargar Archivo TXT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Gestor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(622, 439);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnXML);
             this.Controls.Add(this.btnCargar);
             this.Controls.Add(this.lbDatosRecibidos);
             this.Controls.Add(this.dgvDatosBanco);
@@ -95,6 +121,8 @@
         private System.Windows.Forms.Label lbDatosRecibidos;
         private System.Windows.Forms.Button btnCargar;
         private System.Windows.Forms.OpenFileDialog OFD;
+        private System.Windows.Forms.Button btnXML;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ProyectoPatrono/ProyectoPatrono/Gestor.cs
+++ ProyectoPatrono/ProyectoPatrono/Gestor.cs
@@ -17,6 +17,10 @@ namespace ProyectoPatrono
         private string Excel03ConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
         private string Excel07ConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
 
+        int contExcel = 0;
+        int contXML = 0;
+        int contTXT = 0;
+
         public Gestor()
         {
             InitializeComponent();
@@ -30,6 +34,7 @@ namespace ProyectoPatrono
 
         private void btnCargar_Click(object sender, EventArgs e)
         {
+            contExcel += 1;
             OFD.Title = "Seleccione el archivo de Excel";
             OFD.FileName = string.Empty;
             OFD.Filter = "Archivos de Excel (*.xls;*.xlsx)|*.xls;*.xlsx";
@@ -39,57 +44,88 @@ namespace ProyectoPatrono
 
         private void OFD_FileOk_1(object sender, CancelEventArgs e)
         {
-            string filePath = OFD.FileName;
-            string extension = Path.GetExtension(filePath);
-            string header = "YES";
-            string conStr, sheetName;
+            dgvDatosBanco.DataSource = null;
+            if (contExcel > 0) {
+                string rutaArchivo = OFD.FileName;
+                string extension = Path.GetExtension(rutaArchivo);
+                string header = "YES";
+                string conStr, nombreHoja;
 
-            conStr = string.Empty;
-            switch (extension)
-            {
-
-                case ".xls": //Excel 97-03
-                    conStr = string.Format(Excel03ConString, filePath, header);
-                    break;
-
-                case ".xlsx": //Excel 07
-                    conStr = string.Format(Excel07ConString, filePath, header);
-                    break;
-            }
-
-            //Get the name of the First Sheet.
-            using (OleDbConnection con = new OleDbConnection(conStr))
-            {
-                using (OleDbCommand cmd = new OleDbCommand())
+                conStr = string.Empty;
+                switch (extension)
                 {
-                    cmd.Connection = con;
-                    con.Open();
-                    DataTable dtExcelSchema = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
-                    sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
-                    con.Close();
+
+                    case ".xls": //Excel 97-03
+                        conStr = string.Format(Excel03ConString, rutaArchivo, header);
+                        break;
+
+                    case ".xlsx": //Excel 07
+                        conStr = string.Format(Excel07ConString, rutaArchivo, header);
+                        break;
                 }
-            }
 
-            //Read Data from the First Sheet.
-            using (OleDbConnection con = new OleDbConnection(conStr))
-            {
-                using (OleDbCommand cmd = new OleDbCommand())
+                //Get the name of the First Sheet.
+                using (OleDbConnection con = new OleDbConnection(conStr))
                 {
-                    using (OleDbDataAdapter oda = new OleDbDataAdapter())
+                    using (OleDbCommand cmd = new OleDbCommand())
                     {
-                        DataTable dt = new DataTable();
-                        cmd.CommandText = "SELECT * From [" + sheetName + "]";
                         cmd.Connection = con;
                         con.Open();
-                        oda.SelectCommand = cmd;
-                        oda.Fill(dt);
+                        DataTable dtExcelSchema = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                        nombreHoja = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
                         con.Close();
-
-                        //Populate DataGridView.
-                        dgvDatosBanco.DataSource = dt;
                     }
                 }
-            }
+
+                //Read Data from the First Sheet.
+                using (OleDbConnection con = new OleDbConnection(conStr))
+                {
+                    using (OleDbCommand cmd = new OleDbCommand())
+                    {
+                        using (OleDbDataAdapter oda = new OleDbDataAdapter())
+                        {
+                            DataTable dt = new DataTable();
+                            cmd.CommandText = "SELECT * From [" + nombreHoja + "]";
+                            cmd.Connection = con;
+                            con.Open();
+                            oda.SelectCommand = cmd;
+                            oda.Fill(dt);
+                            con.Close();
+
+                            //Populate DataGridView.
+                            dgvDatosBanco.DataSource = dt;
+                        }
+                    }
+
+                }
+                contExcel = 0;
+            }//fin del if de Excel
+            else if (contXML > 0) {
+                contXML = 0;
+            }//fin del if XML
+            else if (contTXT > 0) {
+                contTXT = 0;
+            }//fin del if de txt
+
+            
+        }//Fin de metodo ok de open file dialog
+
+        private void btnXML_Click(object sender, EventArgs e)
+        {
+            contXML += 1;
+            OFD.Title = "Seleccione el archivo de XML";
+            OFD.FileName = string.Empty;
+            OFD.Filter = "Archivos de XML (*.xml)|*.xml";
+            OFD.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contTXT += 1;
+            OFD.Title = "Seleccione el archivo de TXT";
+            OFD.FileName = string.Empty;
+            OFD.Filter = "Archivos de TXT (*.txt)|*.txt";
+            OFD.ShowDialog();
         }
     }
 }
